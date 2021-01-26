52e8d9efd55f5d58727166449c6bafe51431905f
Nate Tlaseca <nate@whitepantsagency.com>
2021-01-26 10:19:08 -0600

    revert to default serializers

--- web/src/components/serializers.js
+++ web/src/components/serializers.js
@@ -6,24 +6,31 @@ import InstagramEmbed from "react-instagram-embed";
 
 import typography from "./typography.module.css";
 
-const AuthorReference = ({ node }) => {
-  if (node && node.author && node.author.name) {
-    return <span>{node.author.name}</span>;
-  }
-  return <></>;
-};
-
 const serializers = {
   types: {
     block (props) {
       switch (props.node.style) {
+        case 'h1':
+          return <h1 className={typography.responsiveTitle1}>{props.children}</h1>
+
+        case 'h2':
+          return <h2 className={typography.responsiveTitle2}>{props.children}</h2>
+
+        case 'h3':
+          return <h3 className={typography.responsiveTitle3}>{props.children}</h3>
+
+        case 'h4':
+          return <h4 className={typography.responsiveTitle4}>{props.children}</h4>
+
+        case 'blockquote':
+          return <blockquote className={typography.blockQuote}>{props.children}</blockquote>
+
         default:
           return <p className={typography.paragraph}>{props.children}</p>
       }
     },
-    authorReference: AuthorReference,
     mainImage: ({ node }) => <MainImage mainImage={node} />,
-    videoEmbed: ({ node }) => <ReactPlayer className="mt-6 mb-6" url={node.url} controls />,
+    videoEmbed: ({ node }) => <ReactPlayer url={node.url} controls />,
     instagram: ({ node }) => {
       if (!node.url) return null;
       return <InstagramEmbed url={node.url} className="container mx-auto mt-6 mb-6" />;
