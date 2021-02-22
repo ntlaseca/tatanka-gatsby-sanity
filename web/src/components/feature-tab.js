514167e28b0dc1d00b2fa21d78850e12ef74377b
ntlaseca <nate.tlaseca@gmail.com>
2021-02-22 17:41:19 -0600

    first commit

---
+++ web/src/components/feature-tab.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { buildImageObj } from "../lib/helpers";
+import { imageUrlFor } from "../lib/image-url";
+import Container from "./container";
+
+const FeatureTab = () => {
+  <Container>
+
+  </Container>
+}
+
+export default FeatureTab;
