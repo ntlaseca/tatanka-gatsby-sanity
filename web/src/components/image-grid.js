64c38ad205f78437f6f3d34150efcc401b7f3ad4
ntlaseca <nate.tlaseca@gmail.com>
2021-02-26 11:52:01 -0600

    added regex to link to page anchors

--- web/src/components/image-grid.js
+++ web/src/components/image-grid.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "gatsby";
 import { buildImageObj } from '../lib/helpers'
 import { imageUrlFor } from '../lib/image-url'
 
@@ -6,20 +7,22 @@ import styles from "./image-grid.module.css"
 
 function ProfileCard ({ image, categoryLink }) {
   return (
-    <div className={styles.imgContainer}>
-      <div className={styles.textOverlay}>
-        {image.caption}
+    <Link to={`/what-we-do#${image.caption.replace(/\s+/g, '-').toLowerCase()}`}>
+      <div className={styles.imgContainer}>
+        <div className={styles.textOverlay}>
+          {image.caption}
+        </div>
+        {image && image.asset && (
+          <img
+            src={imageUrlFor(buildImageObj(image))
+              .width(720)
+              .height(720)
+              .fit('crop')
+              .url()}
+          />
+        )}
       </div>
-      {image && image.asset && (
-        <img
-          src={imageUrlFor(buildImageObj(image))
-            .width(720)
-            .height(720)
-            .fit('crop')
-            .url()}
-        />
-      )}
-    </div>
+    </Link>
   )
 }
 
